6ea1d8220a4d076d6997ca419fe303d6cde98b51
Hydroxycarbamide <nguyen.huang.eric@gmail.com>
2018-01-19 15:37:46 +0100

    Fonction afficherProgrammeColloque

--- php/affichages.php
+++ php/affichages.php
@@ -30,4 +30,70 @@ function afficherProgramme(){
         <?php
     }
 }
+
+function afficherProgrammeColloque(){
+    global $db;
+    $req = $db->prepare("SELECT interrupteur FROM configs WHERE nom = 'afficherProgramme'");
+    $req->execute();
+    $bool = $req->fetch();
+    if($bool['interrupteur'] == 1){
+        $ateliersDuJour = $db->prepare('SELECT * FROM ateliers WHERE dateA = :dateColloque ORDER BY horaireA, responsableA ASC');
+        $conferencesDuJour = $db->prepare('SELECT * FROM conferences WHERE dateConf = :dateColloque ORDER BY horaireConf ASC');
+        $evenementsDuJour = $db->prepare('SELECT * FROM evenements WHERE dateEvent = :dateColloque ORDER BY horaireEvent ASC');
+        ?>
+        <table class="planing">
+            <tr>
+                <?php
+                $chaqueJourDuCongres = $db->prepare('SELECT * FROM joursColloque');
+                $chaqueJourDuCongres->execute();
+                while ($trouverJour = $chaqueJourDuCongres->fetch()) {
+                    ?>
+                    <th><?php echo convertirDate($trouverJour['dateColloque']); ?></th>
+                    <?php
+                }
+                $chaqueJourDuCongres->closeCursor();
+                ?>
+            </tr>
+            <tr>
+                <?php
+
+                $chaqueJourDuCongres->execute();
+                while ($trouverJour = $chaqueJourDuCongres->fetch()) {
+                    ?>
+                    <td>
+                        <?php
+
+                        # Liste des ateliers du jour
+                        $ACEDuJour = $db->prepare('SELECT horaireA, salleA, titreA, 0 FROM ateliers WHERE dateA = :dateColloque UNION ALL SELECT horaireConf, salleConf, titreConf, 1 FROM conferences WHERE dateConf = :dateColloque ORDER BY horaireA');
+                        $ACEDuJour->execute(array("dateColloque" => $trouverJour['dateColloque']));
+                        while ($trouver = $ACEDuJour->fetch()) {
+                            ?>
+                            <div class="une_liste
+                            <?php
+                            if ($trouver['0'] == 1) {
+                                echo "une_conference";
+                            } else {
+                                echo "un_atelier";
+                            } ?>
+                            ">
+                                <p class="une_liste_details theme" title="<?php echo $trouver['titreA'] ?>"><strong><?php echo trim_text($trouver['titreA'], 50, $ellipses = true, $strip_html = true); ?></strong></p>
+                                <p class="une_liste_details horaire"><?php echo trim_signum($trouver['horaireA']); ?></p>
+                                <p class="une_liste_details salle"><?php echo ucfirst($trouver['salleA']); ?></p>
+                            </div>
+                            <?php
+                        } ?>
+                    </td>
+                <?php
+                }
+            $chaqueJourDuCongres->closeCursor();
+            ?>
+            </tr>
+        </table>
+    <?php
+    } else {
+        ?>
+        <p class = "alert alert-warning"> Programme bientôt en ligne </p>
+        <?php
+    }
+}
 ?>
