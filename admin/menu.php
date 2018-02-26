bd41761171cbfce5f51f397ec3e6c6d214a0f45f
Hydroxycarbamide <nguyen.huang.eric@gmail.com>
2018-02-26 22:27:10 +0100

    Ajout des Marché des Produits

--- admin/menu.php
+++ admin/menu.php
@@ -35,6 +35,7 @@
 						<li><a href="infoP.php#hotels" >Hébergement</a></li>
 						<li><a href="infoP.php#cocktails">Cocktails</a></li>
 						<li><a href="infoP.php#restauration">Diner</a></li>
+						<li><a href="infoP.php#marche">Marché des Produits</a></li>
 						<li><a href="infoP.php#tourisme">Tourisme</a></li>
 						<li><a href="infoP.php#acceswifi">Accès au Wi-Fi</a></li>
 						<li><a href="infoP.php#chartes">Charte de l'IUT et d'UPS</a></li>
